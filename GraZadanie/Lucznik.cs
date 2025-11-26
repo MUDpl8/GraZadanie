67b777d7f5bdebd2534324c05959ba867aa291bd
PKruk <PKruk@DESKTOP-M1DQLDO>
2025-11-26 11:54:43 +0100

    dodano ataki do klasy Lucznik

--- GraZadanie/Lucznik.cs
+++ GraZadanie/Lucznik.cs
@@ -15,14 +15,55 @@ namespace GraZadanie
         {
             this.Imie = imie;  
         }
-        //RZmienna Random do losowania wyników ataku
+        //Zmienna Random do losowania wyników ataku
         Random rnd = new Random();
+        //Zmienna przechowująca stan ataku "Precyzyjny atak"
+        bool wait = false;
         //Różne ataki
         public void Atak1(Bohater cel)//Strzał
         {
+            int obrazenia = rnd.Next(6, 11);
             if (rnd.Next(1, 11) == 10)
             {
-                Console.WriteLine($"Gracz {Imie}");
+                Console.WriteLine($"Gracz {Imie} uzyskał krytyczne trafienie (DMG *2)!");
+                cel.setZycie(obrazenia * 2);
+                Console.WriteLine($"Gracz {Imie} zadał {obrazenia * 2} obrażeń {cel}");
+            }
+            else
+            {
+                cel.setZycie(obrazenia);
+                Console.WriteLine($"Gracz {Imie} zadał {obrazenia} obrażeń {cel}");
+            }
+        }
+        public void Atak2(Bohater cel)//Cięcie sztyletem
+        {
+            int obrazenia = rnd.Next(2, 9);
+            if (rnd.Next(1, 11) >= 7)
+            {
+                Console.WriteLine($"Gracz {Imie} uzyskał krytyczne trafienie (DMG *2)!");
+                cel.setZycie(obrazenia * 2);
+                Console.WriteLine($"Gracz {Imie} zadał {obrazenia * 2} obrażeń {cel}");
+            }
+            else
+            {
+                cel.setZycie(obrazenia);
+                Console.WriteLine($"Gracz {Imie} zadał {obrazenia} obrażeń {cel}");
+            }
+        }
+        public void Atak3(Bohater cel)//Precyzyjny strał
+        {
+            if (wait == false)
+            {
+                Console.WriteLine($"Gracz {Imie} przygotowuje się do precyzyjnego ataku (uzyska grwarantowane krytyczne trafienie)");
+                wait = true;
+            }
+            else if (wait == true)
+            {
+                int obrazenia = rnd.Next(20, 31);
+                Console.WriteLine($"Gracz {Imie} uzyskał krytyczne trafienie (DMG *2)!");
+                cel.setZycie(obrazenia * 2);
+                Console.WriteLine($"Gracz {Imie} zadał {obrazenia * 2} obrażeń {cel}");
+                wait = false;
             }
         }
     }
