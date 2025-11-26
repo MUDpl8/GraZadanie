9892c14b5b2b1eec18dd3dc0f3f90e5486fecc4f
PKruk <PKruk@DESKTOP-M1DQLDO>
2025-11-26 08:25:59 +0100

    klasa główna "Bohater"

---
+++ GraZadanie/Bohater.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraZadanie
+{
+    internal class Bohater
+    {
+        private string Imie;
+        private int Zycie;
+        private int Mana;
+
+        public void setZycie(Bohater cel, int ilosc)
+        {
+            if (cel.Zycie - ilosc <= 0)
+            {
+                cel.Zycie = 0;
+                Console.WriteLine($"Punkty życia gracza: {cel} spadły poniżej zera");
+            }
+            else
+            {
+                cel.Zycie -= ilosc;
+            }
+        }
+    }
+}
