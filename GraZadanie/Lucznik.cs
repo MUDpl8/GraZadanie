e70a6f869d1ba7d0c8d422d7f1fb1faede1c5e7f
PKruk <PKruk@DESKTOP-M1DQLDO>
2025-11-26 08:45:17 +0100

    początak klasy Łucznik

--- GraZadanie/Lucznik.cs
+++ GraZadanie/Lucznik.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime;
 
 namespace GraZadanie
 {
     internal class Lucznik:Bohater
     {
         private string Imie;
+        private string Klasa = "Łucznik";
         public Lucznik(string imie)
         {
-            this.Imie = imie;
+            this.Imie = imie;  
+        }
+        //RZmienna Random do losowania wyników ataku
+        Random rnd = new Random();
+        //Różne ataki
+        public void Atak1(Bohater cel)//Strzał
+        {
+            if (rnd.Next(1, 11) == 10)
+            {
+                Console.WriteLine($"Gracz {Imie}");
+            }
         }
     }
 }
