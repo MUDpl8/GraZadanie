153b003ec3055802da0f844e9a21c1e3c2556bd2
User <User@DESKTOP-CF1AR4D>
2025-11-26 12:22:36 +0100

    Klasa wojownik

---
+++ GraZadanie/Wojownik.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraZadanie
+{
+    internal class Wojownik:Bohater
+    {
+        private string Imie;
+        private string Klasa = "Wojownik";
+        public Wojownik(string imie)
+        {
+            this.Imie = imie;
+        }
+        Random rand = new Random();
+        public void atak_wojownika(Bohater cel)
+        {
+            int obrazenia = rand.Next(7, 11);
+
+            if (rand.Next(1, 5) == 2)
+            {
+                obrazenia = obrazenia * 2;
+                Console.WriteLine($"{Imie} zadał krytyka za {obrazenia} obrażeń");
+            }
+            else
+            {
+                Console.WriteLine($"{Imie} zadał {obrazenia} obrażeń");
+            }
+            cel.setZycie(obrazenia);  
+        }
+        public void silny_atak_wojownika(Bohater cel)
+        {
+            int obrazenia = rand.Next(12,17);
+            if (rand.Next(1, 6) == 2)
+            {
+                Console.WriteLine($"{Imie} zadał {obrazenia} obrażeń");
+                cel.setZycie(obrazenia);
+            }
+            else
+            {
+                Console.WriteLine($"{Imie} nie trafił");
+            }
+        }
+    }
+}
