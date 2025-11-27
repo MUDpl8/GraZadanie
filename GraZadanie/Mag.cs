dc9b270d002d66b683ac5388cdfacd0413fa3b69
Franus <Sala 6@SZBD_SPRZET>
2025-11-27 11:30:35 +0100

    Dodalem cos nowego do maga

--- GraZadanie/Mag.cs
+++ GraZadanie/Mag.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 namespace GraZadanie
 {
-    internal class Mag:Bohater
+    internal class Mag : Bohater
     {
         private string Imie;
         private string Klasa = "Mag";
         private int Zycie = 70;
-        private int Mana = 150;
+        private int Mana = 100;
+        private bool podwojenie = false;
         public Mag(string imie)
         {
             this.Imie = imie;
@@ -35,27 +36,104 @@ namespace GraZadanie
             return Mana;
         }
         Random rnd = new Random();
-        public void KulaMany(Bohater cel)
+        public void KradziezMany(Bohater cel)
         {
             Console.WriteLine("Słaby atak, który kradnie przeciwnikowi część many. Użyć? (T/N)");
             string wybor = Console.ReadLine();
             if (wybor == "T")
             {
-                int obrazenia = rnd.Next(10,16);  //obrażenia 
-                int kradziez = rnd.Next(10, 21);   //kradzież many
-
-                Console.WriteLine($"{Imie} zadał {obrazenia} obrażeń, i ukradł {kradziez} many");
-
-                //zadanie obrażeń przeciwnikowi
-                cel.setZycie(obrazenia);
-
-                //kradzież many od przeciwnika
-                cel.UzyjMane(kradziez);
-
-                //regeneracja many maga
-                this.RegenerujMane(kradziez);
+                if(podwojenie == true)
+                {
+                    int obrazenia = rnd.Next(5, 16)*2;
+                    int kradziez = rnd.Next(10, 21); 
+                    Console.WriteLine($"Błogosławieństwo! {Imie} zadał {obrazenia} obrażeń, i ukradł {kradziez} many");
+                    cel.setZycie(obrazenia);
+                    cel.UzyjMane(kradziez);
+                    this.RegenerujMane(kradziez);
+                    Console.WriteLine($"Mag {Imie} ma {Mana} many");
+                }
+                else
+                {
+                    int obrazenia = rnd.Next(5, 16);
+                    int kradziez = rnd.Next(10, 21);
+                    Console.WriteLine($"Błogosławieństwo! {Imie} zadał {obrazenia} obrażeń, i ukradł {kradziez} many");
+                    cel.setZycie(obrazenia);
+                    cel.UzyjMane(kradziez);
+                    this.RegenerujMane(kradziez);
+                    Console.WriteLine($"Mag {Imie} ma {Mana} many");
+                }
             }
         }
+        public void Kamehameha(Bohater cel)
+        {
+            Console.WriteLine("Silny atak, który kosztuje 35 many. Użyć? (T/N)");
+            string wybor = Console.ReadLine();
+            if (wybor == "T")
+            {
+                if (Mana >= 35)
+                {
+                    if(podwojenie == true)
+                    {
+                        int obrazenia = rnd.Next(15, 31)*2;
+                        Console.WriteLine($"Błogosławieńśtwo! {Imie} zadał {obrazenia} obrażeń");
+                        cel.setZycie(obrazenia);
+                        this.UzyjMane(35);
+                    }
+                    else
+                    {
+                        int obrazenia = rnd.Next(15, 31);
+                        Console.WriteLine($"Błogosławieńśtwo! {Imie} zadał {obrazenia} obrażeń");
+                        cel.setZycie(obrazenia);
+                        this.UzyjMane(35);
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Za mało many! Tracisz turę.");
+                }
 
+            }
+        }
+        public void Błogosławieńśtwo(Bohater cel)
+        {
+            Console.WriteLine($"Następny atak {Imie} zada podwójne obrażenia! Koszt: 15 many. Użyć? (T/N)");
+            string wybor = Console.ReadLine();
+            if (wybor == "T")
+            {
+                if (Mana >= 15)
+                {
+                    podwojenie = true;
+                    this.UzyjMane(15);
+                    Console.WriteLine($"Błogosławieństwo przyznane bohaterowi {Imie}");
+                }
+                else
+                {
+                    Console.WriteLine("Za mało many! Tracisz turę.");
+                }
+            }
+            else
+            {
+                WybierzAtak(cel);
+            }
+        }
+        public void WybierzAtak(Bohater cel)
+        {
+            Console.WriteLine("Proszę wybrać atak do użycia: (1 - Strzał; 2 - Cięcie sztyletam; 3 - Precyzyjny strzał)");
+            int wybor = Convert.ToInt32(Console.ReadLine());
+            switch (wybor)
+            {
+                case 1:
+                    KradziezMany(cel);
+                    break;
+                case 2:
+                    Kamehameha(cel);
+                    break;
+                case 3:
+                    Błogosławieńśtwo(cel);
+                    break;
+            }
+
+        }
     }
 }
--- GraZadanie/Program.cs
+++ GraZadanie/Program.cs
@@ -1,10 +1,12 @@
 ﻿using GraZadanie;
 
-Lucznik Bohater1 = new Lucznik("Piotrek");
+//Lucznik Bohater1 = new Lucznik("Piotrek");
+Mag Bohater1 = new Mag("Krzys");
 Lucznik Bohater2 = new Lucznik("Dummy");
 
 Bohater1.przegladInf();
 Bohater2.przegladInf();
 
-Bohater1.WybierzAtak(Bohater2);
+Bohater1.Kamehameha(Bohater2);
 Bohater2.getZycie();
+Bohater1.getMana();
